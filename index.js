5eb8cbf510784bd5f0dc09ecb90991bc4c15c065
iwa <pierre@nokushi.fr>
2019-12-14 15:37:59 +0100

    fixed #5

--- index.js
+++ index.js
@@ -111,9 +111,14 @@ bot.on('message', async msg => {
             if(lvl != 0) await msg.member.addRole(levels[lvl]);
         }
 
-        return await msg.member.addRole('606862164392673290').then(
+        return await msg.member.addRole('606862164392673290').then(() => {
             msg.delete().catch(console.error)
-        ).catch(console.error)
+            try {
+                msg.member.send({"embed": { "description": "I'm Q-Bot, a unique bot created for this server.\n\nYou can use me with the prefix `?`\nand see all my commands by doing `?help`", "color": 2543500, "author": { "name": "Welcome to Qumu's Discord Server, " + msg.author.username + " !", "icon_url": msg.author.avatarURL}}});
+            } catch (err) {
+                console.log(err)
+            }
+        }).catch(console.error)
     }
     if(!msg.content.startsWith(prefix)) {
 
