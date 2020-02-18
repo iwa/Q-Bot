84d17e329e18a326d86af316242f40f5139c59ab
iwa <pierre@nokushi.fr>
2020-02-18 15:26:14 +0100

    add anime command

--- js/utilities.js
+++ js/utilities.js
@@ -86,42 +86,6 @@ module.exports = class utilities {
 
     }
 
-    static async anime (msg, cont, Discord) {
-
-        if(cont.length < 2)return;
-        cont.shift();
-        var req = cont.join(' ');
-
-        Anilist.search('anime', req, 1, 1).then(async data => {
-            var res = data.media[0];
-            var info = await Anilist.media.anime(res.id)
-            const embed = new Discord.RichEmbed();
-            embed.setTitle("**" + info.title.romaji + " / " + info.title.english + "**")
-            embed.setThumbnail(info.coverImage.large)
-            embed.addField("Status", info.status, true)
-            if(info.episodes != null)
-                embed.addField("Episodes", info.episodes, true)
-            embed.addField("Format", info.format, true)
-            embed.addField("Duration per ep", info.duration + "min", true)
-            embed.addField("Started on", info.startDate.month + "/" + info.startDate.day + "/" + info.startDate.year, true)
-            if(info.endDate.day != null)
-                embed.addField("Ended on", info.endDate.month + "/" + info.endDate.day + "/" + info.endDate.year, true)
-            embed.addField("Genres", info.genres, false)
-            var desc = await info.description.replace(/<br>/gm, '');
-            if(desc.length >= 1024)
-                desc = desc.substring(0, 1023)
-            embed.addField("Description", desc, false)
-            embed.setColor('BLUE')
-
-            console.log(`info: anime request : ${req} by ${msg.author.tag}`)
-            return msg.channel.send(embed)
-        }).catch(err => {
-            console.error(err)
-            return msg.channel.send({'embed': { 'title': ":x: > **An error occured, please retry**" }})
-        });
-
-    }
-
     static async manga (msg, cont, Discord) {
 
         if(cont.length < 2)return;
