48c31249edc4265345f8280edb037b0fd28ef665
iwa <pierre@nokushi.fr>
2020-02-18 14:43:19 +0100

    add sonicsays command + ejs translated file

--- js/memes.js
+++
@@ -1,8 +0,0 @@
-module.exports = class memes {
-
-    static async sonicsays (msg, cont, admin, sonicSays) {
-        if(msg.channel.id != '606165780215889960' && admin.indexOf(msg.author.id) != 0)return;
-        return sonicSays(msg, cont);
-    }
-
-}
