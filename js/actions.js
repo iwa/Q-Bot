989967a8da37ff95b9f766511f5bee10360a8170
iwa <pierre@nokushi.fr>
2020-02-20 11:47:49 +0100

    add multiple users actions

--- js/actions.js
+++ js/actions.js
@@ -27,22 +27,21 @@ module.exports = class actions {
         }
         lastGif[type] = n;
         var r = util.randomInt(reply.length)
+        var mentionFirst, mentionSecond, user
 
         if(args.length == 1) {
             if(msg.mentions.everyone)return;
 
-            var mention = msg.mentions.users.first()
-
-            if(!mention)return;
-
-            if(mention.id == msg.author.id) {
+            mentionFirst = msg.mentions.users.first()
+            if(!mentionFirst)return;
+            if(mentionFirst.id == msg.author.id) {
                 return msg.channel.send({"embed": { "title": `:x: > **You can't ${type} youself !**`, "color": 13632027 }});
             }
 
             const embed = new Discord.RichEmbed();
             embed.setColor('#F2DEB0')
 
-            if(mention.id == '606458989575667732' && type != 'slap') {
+            if(mentionFirst.id == '606458989575667732' && type != 'slap') {
                 setTimeout(() => {
                     r-1;
                     msg.channel.send(reply[r])
@@ -55,17 +54,54 @@ module.exports = class actions {
 
             msg.channel.startTyping()
 
-            embed.setTitle(`**${msg.author.username}** ${type}s you **${mention.username}** !`)
+            embed.setTitle(`**${msg.author.username}** ${type}s you **${mentionFirst.username}** !`)
             embed.setImage(`https://iwa.sh/img/${type}/${n}.gif`)
 
-            var user = await db.get('user').find({ id: msg.author.id }).update(type, n => n + 1).write();
+            user = await db.get('user').find({ id: msg.author.id }).update(type, n => n + 1).write();
 
             embed.setFooter(`you have given ${user[type]} ${type}s`)
 
             return msg.channel.send(embed)
             .then(console.log(`info: ${type} sent by ${msg.author.tag}`), msg.channel.stopTyping(true))
             .catch(console.error);
+        } else if(args.length == 2) {
+            if(msg.mentions.everyone)return;
 
+            mentionFirst = msg.mentions.users.first()
+            mentionSecond = msg.mentions.users.last()
+            if(!mentionFirst || !mentionSecond)return;
+            if(mentionFirst.id == msg.author.id || mentionSecond.id == msg.author.id) {
+                return msg.channel.send({"embed": { "title": `:x: > **You can't ${type} youself !**`, "color": 13632027 }});
+            }
+
+            const embed = new Discord.RichEmbed();
+            embed.setColor('#F2DEB0')
+
+            if((mentionFirst.id == '606458989575667732' || mentionSecond.id == '606458989575667732') && type != 'slap') {
+                setTimeout(() => {
+                    r-1;
+                    msg.channel.send(reply[r])
+                }, 2000)
+            }
+
+            if(msg.channel.type != "dm") {
+                msg.delete().catch(console.error)
+            }
+
+            msg.channel.startTyping()
+
+            embed.setTitle(`**${msg.author.username}** ${type}s you **${mentionFirst.username}** & **${mentionSecond.username}** !`)
+            embed.setImage(`https://iwa.sh/img/${type}/${n}.gif`)
+
+            user = await db.get('user').find({ id: msg.author.id }).update(type, n => n + 2).write();
+
+            embed.setFooter(`you have given ${user[type]} ${type}s`)
+
+            return msg.channel.send(embed)
+            .then(console.log(`info: ${type} sent by ${msg.author.tag}`), msg.channel.stopTyping(true))
+            .catch(console.error);
+        } else if(args.length > 2) {
+            msg.reply(`you can't ${type} them all, you only have 2 arms ! :(`)
         }
     }
 }
