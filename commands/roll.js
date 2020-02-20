4fba9c8b4b3b7904636678154b4cfc50d30356cc
iwa <pierre@nokushi.fr>
2020-02-20 11:15:38 +0100

    make roll output better

--- commands/roll.js
+++ commands/roll.js
@@ -3,11 +3,17 @@ const utils = require('../js/utilities')
 module.exports.run = (bot, msg, args) => {
     if(args.length > 0) {
         var x = args[0]
-        msg.channel.send({"embed": { "title": `**${utils.randomInt(x)}**` }})
+        msg.channel.send({"embed": {
+            "title": `🎲 **${utils.randomInt(x)}**`,
+            "color": 5601658
+        }})
           .then(console.log(`info: roll (${x}) by ${msg.author.tag}`))
           .catch(console.error);
     } else {
-        msg.channel.send({"embed": { "title": `**${utils.randomInt(100)}**` }})
+        msg.channel.send({"embed": {
+            "title": `🎲 **${utils.randomInt(100)}**`,
+            "color": 5601658
+        }})
         .then(console.log(`info: roll (100) by ${msg.author.tag}`))
           .catch(console.error);
     }
