e8ba5757627e8272aeada03f07b2fa3ddf1afef6
Pierre <pierre@nokushi.fr>
2020-03-13 17:00:12 +0100

    update ?leavefan to v12

--- commands/leavefan.js
+++ commands/leavefan.js
@@ -2,9 +2,9 @@ module.exports.run = async (bot, msg) => {
     if(await msg.channel.type != "text")return;
     if(await msg.guild.id != "225359327525994497")return;
     if(await msg.channel.id != "611349541685559316")return;
-    if(await msg.member.roles.find(val => val.id == '613317566261231638')) {
+    if(await msg.member.roles.cache.find(val => val.id == '613317566261231638')) {
         console.log(`info: fans leave: ${msg.author.tag}`)
-        return msg.member.removeRole('613317566261231638').then(msg.reply("you'll not be notified anymore."))
+        return msg.member.roles.remove('613317566261231638').then(msg.reply("you'll not be notified anymore."))
     } else
         return msg.reply("you're already not a Q-Bot fan !")
 };
