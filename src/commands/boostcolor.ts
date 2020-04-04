5a8edccd52cf335cbfe96579a2f7b8eb0fc24d5c
Pierre <pierre@nokushi.fr>
2020-04-04 12:02:55 +0200

    translate ?boostcolor to ts

---
+++ src/commands/boostcolor.ts
@@ -0,0 +1,18 @@
+import { Client, Message } from 'discord.js'
+
+module.exports.run = async (bot:Client, msg:Message) => {
+    if(msg.member.roles.cache.find(val => val.id == process.env.BOOSTROLE)) {
+        if(msg.member.roles.cache.find(val => val.id == process.env.BOOSTCOLOR))
+            return msg.member.roles.remove(process.env.BOOSTCOLOR)
+            .then(() => { msg.reply("You took off your Booster Color !") })
+        else
+            return msg.member.roles.add(process.env.BOOSTCOLOR)
+            .then(() => { msg.reply("You put on your Booster Color !") })
+    }
+};
+
+module.exports.help = {
+    name: 'boostcolor',
+    usage: "?boostcolor",
+    desc: "Put on or put off the Booster Color\n**only usable if you are a Nitro booster**"
+};
