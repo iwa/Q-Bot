20f111d691f508fa59b6634e763220b760260cfb
Pierre <pierre@nokushi.fr>
2020-04-04 15:56:37 +0200

    add ?me as alias of ?profile

---
+++ src/commands/me.ts
@@ -0,0 +1,13 @@
+import { Client, Message } from 'discord.js'
+import { Db } from 'mongodb'
+const profile = require('./profile')
+
+module.exports.run = (bot:Client, msg:Message, args:string[], db:Db) => {
+    profile.run(bot, msg, args, db)
+};
+
+module.exports.help = {
+    name: 'me',
+    usage: "?me [mention someone]",
+    desc: "Print your or someone's profile\n(alias of ?profile)"
+};
