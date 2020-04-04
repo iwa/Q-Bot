6fd88281d64f4cbc08e062c95836b044ca9ff7bd
Pierre <pierre@nokushi.fr>
2020-04-04 10:20:13 +0200

    update index file

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
-const Discord = require('discord.js')
+import * as Discord from "discord.js";
 const bot = new Discord.Client()
-bot.commands = new Discord.Collection();
+const commands = new Discord.Collection();
 
-require('dotenv').config()
-const fs = require('fs');
-const ejs = require('ejs')
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const mongo = require('mongodb').MongoClient;
+import * as fs from 'fs';
+import * as ejs from 'ejs';
+
+import { MongoClient } from 'mongodb';
 const url = process.env.MONGO_URL, dbName = process.env.MONGO_DBNAME;
 
 const letmein = require('./js/letmein')
 const img = require('./js/img')
+const levels = require('../lib/levels.json');
 
-fs.readdir('./commands/', (error, f) => {
+import { YouTube } from 'popyt';
+const yt = new YouTube(process.env.YT_TOKEN)
+
+let cooldown:cooldownType, cooldownXP:cooldownType;
+interface cooldownType {
+	[index:string]: any;
+}
+
+fs.readdir('./build/commands/', (error, f) => {
     if (error) return console.error(error);
     let commandes = f.filter(f => f.split('.').pop() === 'js');
     if (commandes.length <= 0) return console.log('warn: no commands found');
     commandes.forEach((f) => {
         let commande = require(`./commands/${f}`);
-        bot.commands.set(commande.help.name, commande);
+        commands.set(commande.help.name, commande);
     });
 });
-
-let levels = require('./lib/levels.json');
-
-const { YouTube } = require('popyt')
-const yt = new YouTube(process.env.YT_TOKEN)
-
-var cooldown = [], cooldownXP = [];
 
 process.on('uncaughtException', exception => {
     console.error(exception);
@@ -35,17 +39,17 @@ process.on('uncaughtException', exception => {
 
 bot.on('warn', console.warn)
 
-bot.on('error', console.error)
+bot.on('shardError', console.error)
 
-bot.on('disconnect', () => console.log("warn: bot disconnected"))
+bot.on('shardDisconnect', () => console.log("warn: bot disconnected"))
 
-bot.on('reconnecting', async () => {
+bot.on('shardReconnecting', async () => {
     await bot.user.setActivity("Qumu's Remixes | ?help", {type : 2}).catch(console.error);
     await bot.user.setStatus("online").catch(console.error)
     console.log("info: bot reconnecting...")
 });
 
-bot.on('ready', async () => {
+bot.on('shardReady', async () => {
     await bot.user.setActivity("Qumu's Remixes | ?help", {type : 2}).catch(console.error);
     await bot.user.setStatus("online").catch(console.error)
     console.log(`info: logged in as ${bot.user.username}`);
@@ -53,8 +57,9 @@ bot.on('ready', async () => {
 
 // Message Event
 
-bot.on('message', async msg => {
+bot.on('message', async (msg:Discord.Message) => {
 
+    if(!msg)return;
     if(msg.author.bot)return;
     if(msg.channel.type != "text")return;
 
@@ -75,7 +80,7 @@ bot.on('message', async msg => {
         }, 1200000);
     }
 
-    let mongod = await mongo.connect(url, {'useUnifiedTopology': true});
+    let mongod = await MongoClient.connect(url, {'useUnifiedTopology': true});
     let db = mongod.db(dbName);
 
     if(!msg.content.startsWith(process.env.PREFIX)) {
@@ -101,12 +106,12 @@ bot.on('message', async msg => {
 
     let args = msg.content.slice(1).trim().split(/ +/g);
     let req = args.shift();
-    let cmd = bot.commands.get(req);
+    let cmd:any = commands.get(req);
 
     if(req == "letmein")
         return letmein.action(msg, levels, db);
 
-    if(process.env.SLEEP === 1 && msg.author.id != process.env.IWA)return;
+    if(process.env.SLEEP === '1' && msg.author.id != process.env.IWA)return;
 
     if (!cmd) return;
     else cmd.run(bot, msg, args, db);
@@ -128,7 +133,7 @@ bot.on('messageReactionAdd', async reaction => {
         else
             content = `\`\`\`${msg.cleanContent}\`\`\``
 
-        var channel = bot.channels.cache.find(val => val.id == process.env.STARBOARDTC);
+        var channel:any = bot.channels.cache.find(val => val.id == process.env.STARBOARDTC);
 
         await msg.react(reaction.emoji.name);
         await channel.send({
@@ -150,7 +155,7 @@ bot.on('messageReactionAdd', async reaction => {
 });
 
 bot.on('guildMemberRemove', async member => {
-    var mongod = await mongo.connect(url, {'useUnifiedTopology': true});
+    var mongod = await MongoClient.connect(url, {'useUnifiedTopology': true});
     var db = mongod.db(dbName);
 
     var user = await db.collection('user').findOne({ '_id': { $eq: member.id } });
@@ -163,7 +168,7 @@ bot.on('guildMemberRemove', async member => {
 // Subs count, refresh every hour
 
 setInterval(async () => {
-    let channel = bot.channels.cache.find(val => val.id == process.env.SUBCOUNT)
+    let channel:any = bot.channels.cache.find(val => val.id == process.env.SUBCOUNT)
     let title = channel.name
     let newCount = title.replace(/\D/gim, '')
 
@@ -196,16 +201,16 @@ setInterval(async () => {
     if(hh == 7) {
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0');
-        today = mm + '/' + dd;
+        let todayString = `${mm}/${dd}`;
 
-        var mongod = await mongo.connect(url, {'useUnifiedTopology': true});
+        var mongod = await MongoClient.connect(url, {'useUnifiedTopology': true});
         var db = mongod.db(dbName);
 
         var data = await db.collection('user').find({ 'birthday': { $eq: today } }).toArray();
         console.log(data)
 
         if(data.length >= 1) {
-            let channel = bot.channels.cache.find(val => val.id == process.env.BIRTHDAYTC)
+            let channel:any = bot.channels.cache.find(val => val.id == process.env.BIRTHDAYTC)
 
             data.forEach(async user => {
                 var userInfo = await bot.users.fetch(user._id)
@@ -213,7 +218,7 @@ setInterval(async () => {
                 embed.setTitle(`**Happy Birthday, ${userInfo.username} ! 🎉🎉**`)
                 embed.setFooter(`Born on : ${today}`)
                 embed.setColor('#FFFF72')
-                embed.setThumbnail(userInfo.avatarURL)
+                embed.setThumbnail(userInfo.avatarURL({ format: 'png', dynamic: false, size: 128 }))
                 channel.send(`<@${user._id}>`)
                 channel.send(embed)
             });
@@ -227,7 +232,7 @@ bot.login(process.env.TOKEN)
 
 // Functions
 
-async function levelCheck(msg, xp) {
+async function levelCheck(msg:Discord.Message, xp:number) {
     for(var i = 1; i <= 20; i++) {
         if(xp == levels[i].amount) {
             if(i != 1)
@@ -238,8 +243,8 @@ async function levelCheck(msg, xp) {
     }
 }
 
-async function imageLvl(msg, level) {
-    var avatarURL = await msg.author.avatarURL({ format: 'png', dynamic: false, size: 512 })
+async function imageLvl(msg:Discord.Message, level:number) {
+    var avatarURL = msg.author.avatarURL({ format: 'png', dynamic: false, size: 512 })
 
     var html = await ejs.renderFile('views/level.ejs', { avatarURL, level });
     var file = await img.generator(808, 208, html, msg.author.tag, 'lvl')
