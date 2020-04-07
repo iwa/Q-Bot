d9147e03fbb7b26574d8061172e8cc9d16399cfc
Pierre <5201413-iwaQwQ@users.noreply.gitlab.com>
2020-04-07 13:03:20 +0200

    made ?color output RichEmbeds

--- src/commands/color.ts
+++ src/commands/color.ts
@@ -12,24 +12,64 @@ module.exports.run = async (bot:Client, msg:Message, args:string[], db:Db) => {
             let role = msg.member.roles.cache.find(val => colors.indexOf(val.id) > -1)
             if(role) {
                 await msg.member.roles.remove(role)
-                return await msg.reply("you take off your color!")
+                return await msg.channel.send({
+                    "embed": {
+                      "color": 11675878,
+                      "author": {
+                        "name": "You took off your color!",
+                        "icon_url": msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 })
+                      }
+                    }
+                  })
             }
         } else if(Number.isInteger(parseInt(args[0]))) {
             let role = msg.member.roles.cache.find(val => colors.indexOf(val.id) > -1)
             if(role)
                 await msg.member.roles.remove(role)
             let choice:number = parseInt(args[0]) - 1;
-            if(choice > i || choice < 0) return msg.reply(":x: impossible choice")
+            if(choice > i || choice < 0) return msg.channel.send({
+                "embed": {
+                  "color": 14686489,
+                  "author": {
+                    "name": "❌ impossible choice",
+                    "icon_url": msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 })
+                  }
+                }
+              })
             await msg.member.roles.add(colors[choice]);
-            msg.reply(`you're now wearing <@&${colors[choice]}> color!`)
+            msg.channel.send({
+                "embed": {
+                  "description": `You're now wearing <@&${colors[choice]}> color!`,
+                  "color": 11675878,
+                  "author": {
+                    "name": msg.author.username,
+                    "icon_url": msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 })
+                  },
+                  "footer": {
+                      "text": "type `?color off` to take off the color"
+                  }
+                }
+              })
         }
     } else {
-        msg.reply("here are the possible ones :")
         let content:string = "";
         for(let j = 0; i >= j; j++)
-            content = `${content}(${j+1}) <@&${colors[j]}>\n`;
+            content = `${content}**${j+1}.** <@&${colors[j]}>\n`;
 
-        if(content) msg.channel.send(content)
+        if(content) msg.channel.send({
+            "embed": {
+              "title": "Colors you've unlocked :",
+              "description": content,
+              "color": 11675878,
+              "author": {
+                "name": msg.author.username,
+                "icon_url": msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 })
+              },
+              "footer": {
+                  "text": "type `?color (id)` to wear the color you want"
+              }
+            }
+          })
     }
 };
 
