595a3a78962aefc97d54dec0d82ff8e14e9569ca
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-17 15:35:40 +0200

    add 'hifive' alias

---
+++ src/commands/actions/hifive.ts
@@ -0,0 +1,13 @@
+import { Client, Message } from 'discord.js'
+import { Db } from 'mongodb'
+const highfive = require('./highfive');
+
+module.exports.run = async (bot:Client, msg:Message, args:string[], db:Db) => {
+    highfive.run(bot, msg, args, db);
+};
+
+module.exports.help = {
+    name: 'hifive',
+    usage: "?hifive (mention someone)",
+    desc: "Highfive people by mentioning them. Requires the target to react back!\nAlias of ?highfive"
+};
