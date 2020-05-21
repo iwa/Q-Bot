edab1fc37fb8449853d1dd57e9b9067d073f340c
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-21 18:15:40 +0200

    docs: commenting leveling class

--- src/utils/leveling.ts
+++ src/utils/leveling.ts
@@ -1,11 +1,24 @@
+/**
+ * Functions related to leveling up
+ * @packageDocumentation
+ * @module Leveling
+ * @category Utils
+ */
 import * as Discord from "discord.js";
 import * as ejs from 'ejs';
 
+/** @desc Object containing infos about levels (ids, exp amounts, ...) */
 const levels = require('../../lib/levels.json');
 import imGenerator from './img'
 
 export default class leveling {
 
+    /**
+     * Checks if the user has leveled up.
+     * If so, the bot gives the new level role and removes the old one
+     * @param msg - Message object
+     * @param xp - The amount of exp of the user
+     */
     static async levelCheck (msg:Discord.Message, xp:number) {
         for(let i = 1; i <= 20; i++) {
             if(xp == levels[i].amount) {
@@ -17,6 +30,12 @@ export default class leveling {
         }
     }
 
+    /**
+     * Sends the 'level up' image into the same channel
+     * that made the user level up
+     * @param msg - Message object
+     * @param level - Level number of the user
+     */
     static async imageLvl (msg:Discord.Message, level:number) {
         let avatarURL = msg.author.avatarURL({ format: 'png', dynamic: false, size: 512 })
         let cdnUrl = process.env.CDN_URL;
