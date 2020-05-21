c50ffd816e4786e1d0fa823afd0b3b41f5ef309b
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-21 13:01:51 +0200

    commenting memberLeave function

--- src/events/memberLeave.ts
+++ src/events/memberLeave.ts
@@ -1,7 +1,18 @@
+/**
+ * Function that runs every time someone leaves the server
+ * @packageDocumentation
+ * @module MemberLeave
+ * @category Utils
+ */
 import { MongoClient } from 'mongodb';
 import { GuildMember, PartialGuildMember } from 'discord.js';
 const url = process.env.MONGO_URL, dbName = process.env.MONGO_DBNAME;
 
+/**
+ * Called by the 'guildMemberRemove' event
+ * Hids the user in the database (hidden from leaderboards, ...)
+ * @param {GuildMember | PartialGuildMember} member - Guild Member object
+ */
 export default async function memberLeave (member:GuildMember | PartialGuildMember) {
     let mongod = await MongoClient.connect(url, {'useUnifiedTopology': true});
     let db = mongod.db(dbName);
