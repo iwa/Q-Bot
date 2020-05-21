e25dfaa65de214d71136f4f6d652aab08b9298fa
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-21 21:12:17 +0200

    docs: commenting subsCount function

--- src/loops/subsCount.ts
+++ src/loops/subsCount.ts
@@ -1,8 +1,20 @@
+/**
+ * Subs Count function
+ * @packageDocumentation
+ * @module SubsCount
+ * @category Loops
+ */
 import { Client } from 'discord.js';
 import { YouTube } from 'popyt';
 
+/** @desc YouTube API Access */
 const yt = new YouTube(process.env.YT_TOKEN)
 
+/**
+ * Fetches the subs count.
+ * If different than the value on the server, it updates the subs count
+ * @param bot - Discord Client object
+ */
 export default async function subsCount (bot:Client) {
     let channel:any = bot.channels.cache.find(val => val.id == process.env.SUBCOUNT)
     let title = channel.name
