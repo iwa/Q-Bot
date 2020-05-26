f754c42c739b8caaa7b8d09f82df1872af125f66
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-26 19:37:59 +0200

    move ?tronky into its own command file

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -9,7 +9,7 @@ import { Db } from 'mongodb';
 import utilities from './utilities'
 
 /** @desc Automatic replies of the bot when an action is done on it  */
-let reply = ["awww", "thank you :33", "damn you're so precious", "why are you so cute with me ?", "omg", "<3", "so cuuuute c:", "c:", "c;", ":3", "QT af :O", "^u^ thanks!", ">u<", "-u-"] // That "oh yeaaaah ;3" is a little... suggestive. I've changed it. ;) - Hy~
+let reply = ["awww", "thank you :33", "damn you're so precious", "why are you so cute with me ?", "omg", "<3", "so cuuuute c:", "c:", "c;", ":3", "QT af :O", "^u^ thanks!", ">u<", "-u-"]
 
 interface stringKeyArray {
 	[index:string]: any;
@@ -23,8 +23,7 @@ let lastGif:stringKeyArray = {
     'pat': 0,
     'hug': 0,
     'boop': 0,
-    'slap': 0,
-    'tronky': 0
+    'slap': 0
 };
 
 /** define the number of gifs available */
@@ -32,8 +31,7 @@ let count:stringKeyArray = {
     'pat': 46,
     'hug': 47,
     'boop': 15,
-    'slap': 9,
-    'tronky': 0 // How many Tronky gifs do you want on iwa.sh? XD - Hy~
+    'slap': 9
 };
 
 /**
@@ -52,40 +50,26 @@ export default async function actionsRun (bot:Client, msg:Message, args:string[]
     let r = utilities.randomInt(reply.length)
 
     if(args.length <= 2) {
-
         if(msg.mentions.everyone)return;
         let mentionFirst = msg.mentions.users.first()
         let mentionSecond = msg.mentions.users.last()
 
         if(!mentionFirst || !mentionSecond)return;
-        if(mentionFirst.id == msg.author.id || mentionSecond.id == msg.author.id) {
-            if(type == "tronky")
-                return msg.channel.send({"embed": { "title": `:x: > **You can't give yourself a tronky!**`, "color": 13632027 }});
-            else
-                return msg.channel.send({"embed": { "title": `:x: > **You can't ${type} yourself!**`, "color": 13632027 }});
-        }
+        if(mentionFirst.id == msg.author.id || mentionSecond.id == msg.author.id)
+            return msg.channel.send({"embed": { "title": `:x: > **You can't ${type} yourself!**`, "color": 13632027 }});
 
-        if((mentionFirst.id == bot.user.id || mentionSecond.id == bot.user.id) && type != 'slap') {
+        if((mentionFirst.id == bot.user.id || mentionSecond.id == bot.user.id) && type != 'slap')
             setTimeout(() => {
                 r-1;
                 msg.reply(reply[r]);
             }, 2000)
-        }
 
         const embed = new MessageEmbed();
         embed.setColor('#F2DEB0')
-        if(msg.mentions.members.size == 2) {
-            if(type == "tronky")
-                embed.setTitle(`**${msg.author.username}** ${type}s you **${mentionFirst.username}** & **${mentionSecond.username}** !`)
-            else
-                embed.setTitle(`**${msg.author.username}** gave tronkys to **${mentionFirst.username}** & **${mentionSecond.username}** !`)
-	    } else {
-            if(type == "tronky")
-                embed.setTitle(`**${msg.author.username}** gave a tronky to **${mentionFirst.username}** !`)
-            else
-                embed.setTitle(`**${msg.author.username}** ${type}s you **${mentionFirst.username}**!`)
-        }
-
+        if(msg.mentions.members.size == 2)
+            embed.setTitle(`**${msg.author.username}** ${type}s you **${mentionFirst.username}** & **${mentionSecond.username}**!`)
+	    else
+            embed.setTitle(`**${msg.author.username}** ${type}s you **${mentionFirst.username}**!`)
         embed.setImage(`https://${process.env.CDN_URL}/img/${type}/${n}.gif`)
 
         let user = await db.collection('user').findOne({ '_id': { $eq: msg.author.id } });
@@ -102,11 +86,7 @@ export default async function actionsRun (bot:Client, msg:Message, args:string[]
     } else if(args.length > 2) {
         if(type == "hug") {
             msg.reply(`You can't ${type} them all, you can't fit more than two people in a hug! :(`)
-        }
-        else if(type == "tronky") {
-            msg.reply("Only two tronkys at a time! :(")
-        }
-        else {
+        } else {
             msg.reply(`You can't ${type} them all, you only have 2 arms! :(`)
         }
     }
