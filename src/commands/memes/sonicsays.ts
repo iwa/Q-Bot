c81f9662a76213616b504a72c6330fcdc1a2d9b5
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-27 11:15:32 +0200

    fix TS formatting in ?sonicsays

--- src/commands/memes/sonicsays.ts
+++ src/commands/memes/sonicsays.ts
@@ -2,9 +2,9 @@ import { Client, Message } from 'discord.js'
 import * as ejs from 'ejs'
 import imGenerator from '../../utils/img';
 
-module.exports.run = async (bot:Client, msg:Message, args:string[]) => {
-    if(msg.channel.id != '606165780215889960') return msg.channel.send({"embed": { "title": `:x: > **Command only usable in #memes**`, "color": 13632027}});
-    if(args.length > 0) {
+module.exports.run = async (bot: Client, msg: Message, args: string[]) => {
+    if (msg.channel.id != '606165780215889960') return msg.channel.send({ "embed": { "title": `:x: > **Command only usable in #memes**`, "color": 13632027 } });
+    if (args.length > 0) {
         msg.channel.startTyping();
         let parole = args;
         let x = parole.join(' ')
@@ -16,9 +16,9 @@ module.exports.run = async (bot:Client, msg:Message, args:string[]) => {
 
         try {
             console.log(`info: sonicsays by ${msg.author.tag}`)
-            return msg.channel.send('', {files: [file]})
+            return msg.channel.send('', { files: [file] })
                 .then(() => { msg.channel.stopTyping(true) });
-        } catch(err) {
+        } catch (err) {
             console.error(err)
         }
     }
