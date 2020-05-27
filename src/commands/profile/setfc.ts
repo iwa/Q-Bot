47c00f40891c629aa27925caa9636e62c9f118a9
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-27 11:25:55 +0200

    fix TS formatting in profile commands

--- src/commands/profile/setfc.ts
+++ src/commands/profile/setfc.ts
@@ -1,19 +1,19 @@
 import { Client, Message, MessageEmbed } from 'discord.js'
 import { Db } from 'mongodb'
 
-module.exports.run = async (bot:Client, msg:Message, args:string[], db:Db) => {
-    if(args.length == 1) {
+module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db) => {
+    if (args.length == 1) {
         let content = args[0]
-        if(content.length != 14 || content.search(/\d\d\d\d-\d\d\d\d-\d\d\d\d/gi) == -1) {
-            return msg.channel.send({"embed": { "title": ":x: > **Switch Friend Code format invalid! Please enter your FC without the 'SW-' at the beginning**", "color": 13632027 }});
+        if (content.length != 14 || content.search(/\d\d\d\d-\d\d\d\d-\d\d\d\d/gi) == -1) {
+            return msg.channel.send({ "embed": { "title": ":x: > **Switch Friend Code format invalid! Please enter your FC without the 'SW-' at the beginning**", "color": 13632027 } });
         }
 
         let userDB = await db.collection('user').findOne({ '_id': { $eq: msg.author.id } });
-        if(userDB.fc != null) {
-            return msg.channel.send({"embed": { "title": ":x: > **Sorry, you can't change your FC!**", "description": "**Please contact <@125325519054045184> to change.**", "color": 13632027 }});
+        if (userDB.fc != null) {
+            return msg.channel.send({ "embed": { "title": ":x: > **Sorry, you can't change your FC!**", "description": "**Please contact <@125325519054045184> to change.**", "color": 13632027 } });
         }
 
-        await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { fc: content }}, { upsert: true });
+        await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { fc: content } }, { upsert: true });
         const embed = new MessageEmbed();
         embed.setAuthor("Your Switch FC is now set to : ", msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 }));
         embed.setTitle(`**${content}**`)
@@ -21,7 +21,7 @@ module.exports.run = async (bot:Client, msg:Message, args:string[], db:Db) => {
         try {
             console.log(`info: switch fc of ${msg.author.tag} set`)
             return await msg.channel.send(embed);
-        } catch(err) {
+        } catch (err) {
             console.error(err);
         }
     }
--- src/commands/profile/setpsn.ts
+++ src/commands/profile/setpsn.ts
@@ -1,17 +1,17 @@
 import { Client, Message, MessageEmbed } from 'discord.js'
 import { Db } from 'mongodb'
 
-module.exports.run = async (bot:Client, msg:Message, args:string[], db:Db) => {
-    if(args.length == 1) {
+module.exports.run = async (bot: Client, msg: Message, args: string[], db: Db) => {
+    if (args.length == 1) {
         let content = args[0]
-        if(content.length > 16 || content.length < 3)
-            return msg.channel.send({"embed": { "title": ":x: > **PSN ID format invalid!**", "color": 13632027 }});
+        if (content.length > 16 || content.length < 3)
+            return msg.channel.send({ "embed": { "title": ":x: > **PSN ID format invalid!**", "color": 13632027 } });
 
         let userDB = await db.collection('user').findOne({ '_id': { $eq: msg.author.id } });
-        if(userDB.psn != null)
-            return msg.channel.send({"embed": { "title": ":x: > **Sorry, you can't change your PSN ID!**", "description": "**Please contact <@125325519054045184> to change.**", "color": 13632027 }});
+        if (userDB.psn != null)
+            return msg.channel.send({ "embed": { "title": ":x: > **Sorry, you can't change your PSN ID!**", "description": "**Please contact <@125325519054045184> to change.**", "color": 13632027 } });
 
-        await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { psn: content }}, { upsert: true });
+        await db.collection('user').updateOne({ _id: msg.author.id }, { $set: { psn: content } }, { upsert: true });
         const embed = new MessageEmbed();
         embed.setAuthor("Your PSN ID is now set to : ", msg.author.avatarURL({ format: 'png', dynamic: false, size: 128 }));
         embed.setTitle(`**${content}**`)
@@ -19,7 +19,7 @@ module.exports.run = async (bot:Client, msg:Message, args:string[], db:Db) => {
         try {
             console.log(`info: psn id of ${msg.author.tag} set`)
             return await msg.channel.send(embed);
-        } catch(err) {
+        } catch (err) {
             console.error(err);
         }
     }
