747b2180d15f27b654d37a61df63007b806e96fa
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-28 14:02:23 +0200

    update ?tornky to match new actions algorithm

--- src/commands/actions/tronky.ts
+++ src/commands/actions/tronky.ts
@@ -4,44 +4,42 @@
 import { Client, Message, MessageEmbed } from 'discord.js'
 
 module.exports.run = async (bot: Client, msg: Message, args: string[]) => {
-    if (args.length <= 3) { // There's three arguments here: the two people to give tronkys to, and the flavour.
+    if (args.length <= 11) {
         if (msg.mentions.everyone) return;
-        let mentionFirst = msg.mentions.users.first() // Variable stuff here.
-        let mentionSecond = msg.mentions.users.last() // This just sets to the people who were @'d in the message.
+        if (!msg.mentions.members) return;
 
         // Define and decide the flavour of the tronky. Hazelnut is the default and is used if no or invalid flavour provided.
-        let flavour: [string] = ['hazelnut']
-        if (args.length == 3) {
-            if ('cocoa' in args) flavour = ['cocoa']
-            if ('milk' in args) flavour = ['milk']
-            if ('cereal' in args) flavour = ['cereal']
-            if ('pistachio' in args) flavour = ['pistacio']
-        }
-        
+        let flavour: string = 'hazelnut'
+        if ('cocoa' in args) flavour = 'cocoa'
+        if ('milk' in args) flavour = 'milk'
+        if ('cereal' in args) flavour = 'cereal'
+        if ('pistachio' in args) flavour = 'pistacio'
 
-        if (!mentionFirst || !mentionSecond) return; // If it isn't a mention, don't continue
-        if (mentionFirst.id == msg.author.id || mentionSecond.id == msg.author.id) // If mention is person who sent message, send this
+        if (msg.mentions.members.has(msg.author.id)) // If mention is person who sent message, send this
             return msg.channel.send({ "embed": { "title": `:x: > **You can't give yourself a tronky!**`, "color": 13632027 } });
 
-        if ((mentionFirst.id == bot.user.id || mentionSecond.id == bot.user.id)) // If mention is Q-Bot, give a tronky back ^u^
+        if (msg.mentions.members.has(bot.user.id)) // If mention is Q-Bot, give a tronky back ^u^
             setTimeout(() => {
                 msg.reply('<:tronkyForYou:712980723258097664>');
             }, 2000)
 
         const embed = new MessageEmbed();
         embed.setColor('#F2DEB0')
-        if (msg.mentions.members.size == 2) // Send the embed for two people
-            embed.setDescription(`**<@${msg.author.id}>** gave **${flavour}** tronkys to **<@${mentionFirst.id}>** & **<@${mentionSecond.id}>**! <:tronkyForYou:712980723258097664><:tronkyForYou:712980723258097664>`)
-        else // Send the embed for one person
-            embed.setDescription(`**<@${msg.author.id}>** gave a **${flavour}** tronky to **<@${mentionFirst.id}>**! <:tronkyForYou:712980723258097664>`)
+
+        if (msg.mentions.members.size >= 2) {
+            let users = msg.mentions.members.array()
+            let title: string = `**<@${msg.author.id}>** gave **${flavour}** tronkys to **<@${users[0].id}>**`;
+            for (let i = 1; i < (msg.mentions.members.size - 1); i++)
+                title = `${title}, **<@${users[i].id}>**`
+            title = `${title} & **<@${(msg.mentions.members.last()).id}>**! <:tronkyForYou:712980723258097664><:tronkyForYou:712980723258097664><:tronkyForYou:712980723258097664>`
+            embed.setDescription(title);
+        } else
+            embed.setDescription(`**<@${msg.author.id}>** gave a **${flavour}** tronky to **<@${(msg.mentions.members.first()).id}>**! <:tronkyForYou:712980723258097664>`)
 
         return msg.channel.send(embed)
-            .then(() => { // Log into the console.
-                console.log(`info: tronky sent by ${msg.author.tag}`);
-            })
+            .then(() => { console.log(`info: tronky sent by ${msg.author.tag}`); })
             .catch(console.error);
-    } else if (args.length > 3) // Kinda similar to how actions used to work before where you could only action up to two people.
-        msg.reply("Only two tronkys at a time! :(")
+    }
 };
 
 module.exports.help = { // Define the info to display in the ?help command.
