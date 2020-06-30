783407e5a4f46541a071622a9f34c29608372e9d
Pierre <19956672+iwa@users.noreply.github.com>
2020-06-30 23:13:56 +0200

    fix iwa and Q perms on suggestion commands

--- src/commands/approve.ts
+++ src/commands/approve.ts
@@ -2,7 +2,7 @@ import { Client, Message, TextChannel, MessageEmbed } from 'discord.js';
 import utilities from '../utils/utilities';
 
 module.exports.run = async (bot: Client, msg: Message, args: string[]) => {
-    if (!utilities.isMod(msg)) return;
+    if (!utilities.isMod(msg) && msg.author.id != process.env.IWA && msg.author.id != process.env.QUMU) return;
 
     let channel = await bot.channels.fetch(process.env.SUGGESTIONTC);
     let suggestion = await (channel as TextChannel).messages.fetch(args[0])
@@ -26,8 +26,10 @@ module.exports.run = async (bot: Client, msg: Message, args: string[]) => {
     embedDM.setDescription(`Check it out [here](${suggestion.url})`)
 
     for(const user of users.array()) {
-        if(!user.bot)
+        if(!user.bot) {
             await user.send(embedDM).catch(() => {return});
+            console.log('yo')
+        }
     }
 
     await suggestion.edit(embed);
